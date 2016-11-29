94f8e612255378302cca8874dceb5b3831de5744
sparksjj <e.gursky@mobiles.by>
2016-11-29 22:10:56 +0300

    ad gm to contact page

--- app/Http/Controllers/Site/ContactsController.php
+++ app/Http/Controllers/Site/ContactsController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Site;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Cornford\Googlmapper\Facades\MapperFacade as Mapper;
 
 class ContactsController extends Controller
 {
     public function index(){
+    	Mapper::map(53.381128999999990000, -1.470085000000040000);
     	return view('site.contact');
     }
 }
