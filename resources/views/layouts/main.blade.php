dc6348c6304385d331650af3712dfbcd1967b25e
sparksjj <e.gursky@mobiles.by>
2016-12-09 16:12:40 +0300

    add source show on site

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,28 +14,28 @@
 <meta name="format-detection" content="telephone=no">
 <!-- CSS
   ================================================== -->
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-<link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css">
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<link href="vendor/prettyphoto/css/prettyPhoto.css" rel="stylesheet" type="text/css">
-<link href="vendor/owl-carousel/css/owl.carousel.css" rel="stylesheet" type="text/css">
-<link href="vendor/owl-carousel/css/owl.theme.css" rel="stylesheet" type="text/css">
+<link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="/css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link href="/vendor/prettyphoto/css/prettyPhoto.css" rel="stylesheet" type="text/css">
+<link href="/vendor/owl-carousel/css/owl.carousel.css" rel="stylesheet" type="text/css">
+<link href="/vendor/owl-carousel/css/owl.theme.css" rel="stylesheet" type="text/css">
 <!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
-<link href="css/custom.css" rel="stylesheet" type="text/css"><!-- CUSTOM STYLESHEET FOR STYLING -->
+<link href="/css/custom.css" rel="stylesheet" type="text/css"><!-- CUSTOM STYLESHEET FOR STYLING -->
 
     <!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
-    <link rel="stylesheet" type="text/css" href="css/extralayers.css" media="screen" /> 
-    <link rel="stylesheet" type="text/css" href="vendor/revslider/css/settings.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="/css/extralayers.css" media="screen" /> 
+    <link rel="stylesheet" type="text/css" href="/vendor/revslider/css/settings.css" media="screen" />
 
     <!-- GOOGLE FONTS FOR REVOLUTION SLIDER ONLY -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800' rel='stylesheet' type='text/css' />
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css' />
     <link href='http://fonts.googleapis.com/css?family=Raleway:100,200,300,700,800,900' rel='stylesheet' type='text/css' />
 <!-- Color Style -->
-<link href="colors/color1.css" rel="stylesheet" type="text/css">
+<link href="/colors/color1.css" rel="stylesheet" type="text/css">
 <!-- SCRIPTS
   ================================================== -->
-<script src="js/modernizr.js"></script><!-- Modernizr -->
+<script src="/js/modernizr.js"></script><!-- Modernizr -->
 </head>
 <body class="home header-style1">
 
@@ -47,19 +47,19 @@
         @include('layouts.parts._footer2')
         <a id="back-to-top" style="z-index: 100;"><i class="fa fa-chevron-up"></i></a> 
     </div>
-<script src="js/jquery-2.1.3.min.js"></script> <!-- Jquery Library Call -->
-<script src="vendor/prettyphoto/js/prettyphoto.js"></script> <!-- PrettyPhoto Plugin -->
-<script src="js/ui-plugins.js"></script> <!-- UI Plugins -->
-<script src="js/helper-plugins.js"></script> <!-- Helper Plugins -->
-<script src="vendor/owl-carousel/js/owl.carousel.min.js"></script> <!-- Owl Carousel -->
-<script src="js/bootstrap.js"></script> <!-- UI -->
-<script src="js/init.js"></script> <!-- All Scripts -->
-<script src="vendor/flexslider/js/jquery.flexslider.js"></script> <!-- FlexSlider -->
+<script src="/js/jquery-2.1.3.min.js"></script> <!-- Jquery Library Call -->
+<script src="/vendor/prettyphoto/js/prettyphoto.js"></script> <!-- PrettyPhoto Plugin -->
+<script src="/js/ui-plugins.js"></script> <!-- UI Plugins -->
+<script src="/js/helper-plugins.js"></script> <!-- Helper Plugins -->
+<script src="/vendor/owl-carousel/js/owl.carousel.min.js"></script> <!-- Owl Carousel -->
+<script src="/js/bootstrap.js"></script> <!-- UI -->
+<script src="/js/init.js"></script> <!-- All Scripts -->
+<script src="/vendor/flexslider/js/jquery.flexslider.js"></script> <!-- FlexSlider -->
 <!-- <script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script> -->
   
 <!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
-<script type="text/javascript" src="vendor/revslider/js/jquery.themepunch.tools.min.js"></script>   
-<script type="text/javascript" src="vendor/revslider/js/jquery.themepunch.revolution.min.js"></script>
+<script type="text/javascript" src="/vendor/revslider/js/jquery.themepunch.tools.min.js"></script>   
+<script type="text/javascript" src="/vendor/revslider/js/jquery.themepunch.revolution.min.js"></script>
 <script type="text/javascript">
 
     document.addEventListener("DOMContentLoaded", function() {
@@ -135,6 +135,6 @@
         });             
     }); //ready
 </script>
-<script src="js/custom.js"></script> <!-- All Scripts -->
+<script src="/js/custom.js"></script> <!-- All Scripts -->
 </body>
 </html>
