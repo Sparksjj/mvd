c30bbb8bc7cb1eb68d3b157f4165cbc19e9daea8
sparksjj <e.gursky@mobiles.by>
2017-02-16 22:28:54 +0300

    add device vith slider

---
+++ resources/views/site/devices/device1.blade.php
@@ -0,0 +1,371 @@
+<!DOCTYPE HTML>
+<html class="no-js">
+<head>
+<!-- Basic Page Needs
+  ================================================== -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Vestige - Responsive HTML5 Template</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<!-- CSS
+  ================================================== -->
+<link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="/css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link href="/vendor/prettyphoto/css/prettyPhoto.css" rel="stylesheet" type="text/css">
+<link href="/vendor/owl-carousel/css/owl.carousel.css" rel="stylesheet" type="text/css">
+<link href="/vendor/owl-carousel/css/owl.theme.css" rel="stylesheet" type="text/css">
+<!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
+<link href="/css/custom.css" rel="stylesheet" type="text/css"><!-- CUSTOM STYLESHEET FOR STYLING -->
+<!-- Color Style -->
+<link href="/colors/color1.css" rel="stylesheet" type="text/css">
+<link href="/FlexSlider/flexslider.css" rel="stylesheet" type="text/css">
+<!-- SCRIPTS
+  ================================================== -->
+</head>
+<body class="header-style1">
+<!--[if lt IE 7]>
+	<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+<![endif]-->
+<div class="body">
+<style>
+    .nav-pills>li+li{
+        margin-left: 10px;
+    }
+    .main div#container{
+        padding: 0;
+        margin: 0;
+    }
+    body,
+    .main,
+    .content,
+    .content > div,
+    .container,
+    .tab-content,
+    .tab-pane,
+    .modal-content,
+    .modal-body{
+        height: 100% !important;
+    }
+    .tab-content{
+        padding: 0;
+        background-color: transparent;
+        border: none;
+    }
+    .content{
+        padding: 0;
+    }
+    .hero-area{
+        height: 195px;
+        min-height: 100px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+    .main{
+        padding-top: 195px;
+    }
+    #content_2d > .container{
+        display: flex;
+        align-items: center;
+        height: 100%;
+        justify-content: center;
+    }
+    #content_2d > .container .flex_img{
+        width: 50%;
+        padding: 10px;
+        border: 1px solid #999;
+        display: block;
+        margin-right: 20px;
+        border-radius: 20px;
+        margin-bottom: 150px;
+        cursor: pointer;
+        position: relative;
+    }
+    #content_2d > .container .flex_img:last-child{
+        margin-right: 0px;
+    }
+    .full-width-img{
+        width: auto;
+        max-width: 100%;
+        height: 100%;
+        display: block;
+        margin: 0 auto;
+        padding: 10px;
+        padding-bottom: 60px;
+    }/* 
+    .flex_img:before{
+        content: "\f06e";
+        font: normal normal normal 14px/1 FontAwesome;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-left: -30px;
+        margin-top: -30px;
+        font-size: 60px;
+        opacity: .5;
+        text-shadow: 0 0 10px #000;
+        color: #dabc74;
+    } */
+    .carousel-control.right,
+    .carousel-control.left{
+        background: transparent;
+    }
+    .item{
+        padding: 0 50px;
+        text-align: center;
+    }
+    .item img{
+        margin: 0 auto;
+        max-width: 100%;
+    }
+    .carousel-inner{
+        margin-top: 10%;
+    }
+</style>
+    <div class="hero-area">
+    	<div class="page-header">
+            <div style="height: auto">
+                <div style="vertical-align: top">
+                    <span style="margin-top: 30px; color: #222; background-color: #eee; border-radius: 4px; padding: 15px ">Планшет в холле</span>
+                    <div class="grid-filter" style="margin: 30px">
+                        <ul class="nav nav-pills sort-source" data-sort-id="gallery" data-option-key="filter">
+                            <li data-option-value="*" class="active"><a href="#content_2d" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-map-o"></i> <span>Схема музея</span></a></li>
+                            <li data-option-value=".format-image"><a href="#3d" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-picture-o"></i> <span>3D тур</span></a></li>
+                            <li data-option-value=".format-link"><a href="#some_slide" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-link"></i> <span>some_slide</span></a></li>
+                            <li data-option-value=".format-gallery"><a href="#"><i class="fa fa-camera"></i> <span>Sliders</span></a></li>
+                            <li data-option-value=".format-video"><a href="#"><i class="fa fa-play"></i> <span>Videos</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Start Body Content -->
+  	<div class="main" role="main">
+    	<div id="content" class="content full">
+
+              <div class="tab-content">
+                <div role="tabpanel" class="tab-pane active" id="content_2d">
+                    <div class="container">
+                        <div class="flex_img">                            
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" alt="" class="show-more" data-src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg">
+                        </div>
+                        <div class="flex_img">                            
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" alt="" class="show-more" data-src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg">
+                        </div>
+                    </div>
+                </div>
+                <div role="tabpanel" class="tab-pane" id="3d">@include('layouts.MVD')</div>
+                <div role="tabpanel" class="tab-pane" id="some_slide" style="display: block">
+                <style>
+                    .slider .slides,
+                    .slider .slides ,
+                    .slider li,
+                    .flex-viewport,
+                    #slider{
+                        height: 100% !important;
+                    }
+
+                    .slider img
+                    {
+                        max-height: 100%;
+                    }
+                    #carousel .flex-direction-nav  a,
+                    #slider .flex-direction-nav  a{
+                        display: block;
+                        opacity: 1 !important;
+                    }
+                    .flexslider:hover .flex-direction-nav .flex-prev{
+                        left: -120px;
+                    }
+                    .flexslider:hover .flex-direction-nav .flex-next{
+                        right: -100px;
+                    }
+                    .flexslider .flex-prev, .flexslider .flex-next{
+                        background: transparent;
+                        height: 50px;
+                        padding: 30px;
+                        top: 35%;
+                    }
+                    .flex-direction-nav .flex-next{
+                        right: -100px;
+                    }
+                    .flex-direction-nav .flex-prev{
+                        left: -120px;
+                    }
+                    #slider .flex-direction-nav .flex-disabled,
+                    #carousel .flex-direction-nav .flex-disabled{
+                        opacity: .2 !important;
+                    }
+                    .flexslider .flex-prev:before{
+                        content: '\f001';
+                    }
+                    .flexslider .slides img{
+                        width: auto;
+                        display: block;
+                        margin: 0 auto;
+                    }
+                    .flexslider .slides > li{
+                        display: flex !important;
+                        align-items: center;
+                    }
+                </style>
+                <div class="container">
+                  <section class="slider" style="height: 100%; position: relative">
+                    <div id="slider" class="flexslider" style="padding-bottom: 190px; max-height: 100%">
+                      <ul class="slides" style="">
+                        <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://ww1.prweb.com/prfiles/2013/04/26/10781475/Siberian%20Cedars.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://anywalls.com/pic/201210/1920x1080/anywalls.com-2511.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://fb.ru/misc/i/gallery/35703/890019.jpg" />
+                            </li>
+                        <li>
+                            <img src="http://vignette2.wikia.nocookie.net/harrypotter/images/4/4a/%D0%9B%D0%B5%D1%81.jpg/revision/latest?cb=20140626150601&path-prefix=ru" />
+                            </li>
+                            <li>
+                            <img src="http://i21.beon.ru/86/90/1429086/45/60548845/0.jpeg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                        <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                      </ul>
+                    </div>
+                    <div id="carousel" class="flexslider" style="position: absolute;bottom: 0;left: 0;margin: 20px 0;right: 0;">
+                      <ul class="slides" style="
+    display: flex;
+    align-items: center;">
+                        <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://ww1.prweb.com/prfiles/2013/04/26/10781475/Siberian%20Cedars.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://anywalls.com/pic/201210/1920x1080/anywalls.com-2511.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://fb.ru/misc/i/gallery/35703/890019.jpg" />
+                            </li>
+                        <li>
+                            <img src="http://vignette2.wikia.nocookie.net/harrypotter/images/4/4a/%D0%9B%D0%B5%D1%81.jpg/revision/latest?cb=20140626150601&path-prefix=ru" />
+                            </li>
+                            <li>
+                            <img src="http://i21.beon.ru/86/90/1429086/45/60548845/0.jpeg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                        <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                            <li>
+                            <img src="http://nsk.novosibdom.ru/story/NOVOSIBIRSK/NOVONIK_1910-1920/school_05.jpg" />
+                            </li>
+                      </ul>
+                    </div>
+                  </section>
+                    
+                </div>
+
+                </div>
+                <div role="tabpanel" class="tab-pane" id="settings">4444444444</div>
+                <div role="tabpanel" class="tab-pane" id="settings">5555555555</div>
+              </div>
+
+   		</div>
+      </div>
+</div>
+
+<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg" style="width: 90%; height: 93%" role="document">
+    <div class="modal-content">
+      <div class="modal-content">
+      <div class="modal-header" style="padding-bottom: 30px; border: none">
+        <button type="button" class="close" style="font-size: 30px; opacity: .9; padding: 10px 20px;margin-top: -15px" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+      <div class="modal-body">
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="/js/jquery-2.1.3.min.js"></script> <!-- Jquery Library Call -->
+<script src="/js/bootstrap.js"></script> <!-- UI -->
+<script defer src="/FlexSlider/jquery.flexslider-min.js"></script>
+<script>
+    addEventListener('load', function(){
+        $('.show-more').on('click', function(e){
+            var src = $(e.target).attr('data-src');
+            $('.modal .modal-content .modal-body').empty();
+            $('<img src="'+src+'" class="full-width-img" alt="" />').appendTo($('.modal .modal-content .modal-body'));
+            $('.modal').modal('show')
+        })
+      $('#carousel').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        asNavFor: '#slider'
+      });
+
+      $('#slider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+      $('#slider .slides li').on('click', function(e){
+        var target = $(e.target);
+        if (!target.is('img')) {
+            target = target.find('img');
+        }
+            var src = target.attr('src');
+            $('.modal .modal-content .modal-body').empty();
+            $('<img src="'+src+'" class="full-width-img" alt="" />').appendTo($('.modal .modal-content .modal-body'));
+            $('.modal').modal('show')
+      })
+    })
+</script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/contacts', 'Site\ContactsController@index')->name('contacts.index')
 Route::get('/about', 'Site\AboutController@index')->name('about.index');
 Route::get('/3d', 'Site\ThreeController@index')->name('3d.index');
 Route::get('/search', 'Site\SearchController@index')->name('search.index');
+Route::get('/device1', function(){
+	return view('site.devices.device1');
+});
 Route::resource('resource',  'Site\ResourceController', ['only' => ['show', 'index']]);
 
 Auth::routes();
