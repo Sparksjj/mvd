ed9dee67a158ad5af6f4c624a5bdad0228c049f1
Yauhen <Arucard2009@yandex.ru>
2017-02-21 22:58:25 +0300

    add images

--- resources/views/site/parts/_slider.blade.php
+++ resources/views/site/parts/_slider.blade.php
@@ -6,21 +6,21 @@
         		<ul style="display:none;">
 
                     <li data-transition="fade" data-slotamount="1" data-masterspeed="100"  data-saveperformance="off"  data-title="{{trans('layout.slide')}} 1">
-                        <img src="/images/slider1.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
+                        <img src="/images/slider2.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
                     </li>
                     <li data-transition="fade" data-slotamount="1" data-masterspeed="100"  data-saveperformance="off"  data-title="{{trans('layout.slide')}} 2">
-                        <img src="/images/slider1.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
+                        <img src="/images/slider3.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
                     </li>
                     <li data-transition="fade" data-slotamount="1" data-masterspeed="100"  data-saveperformance="off"  data-title="{{trans('layout.slide')}} 3">
-                        <img src="/images/slider1.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
+                        <img src="/images/slider4.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
                     </li>
                     <li data-transition="fade" data-slotamount="1" data-masterspeed="100"  data-saveperformance="off"  data-title="{{trans('layout.slide')}} 4">
+                        <img src="/images/slider6.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
+                    </li>
+                    <li data-transition="fade" data-slotamount="1" data-masterspeed="100"  data-saveperformance="off"  data-title="{{trans('layout.slide')}} 5">
                         <img src="/images/slider1.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
                     </li>
-                    <li data-transition="fade" data-slotamount="1" data-masterspeed="100"  data-saveperformance="off"  data-title="{{trans('layout.slide')}} 6">
-                        <img src="/images/slider1.jpg"  alt="fullslide2"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
-                    </li>
-                    
+
             	</ul>	
             	<div class="tp-bannertimer" style="display:none;"></div>	
         	</div>
