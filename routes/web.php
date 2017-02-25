25389a463a9b7efc3da34e1788a63e4141a64a13
sparksjj <e.gursky@mobiles.by>
2017-02-25 23:26:40 +0300

    asda

--- routes/web.php
+++ routes/web.php
@@ -149,7 +149,7 @@ Route::get('parser', function(){
                 		foreach( $files_url as $index=>$value){                			
 				        	$fileext = pathinfo($value)['extension'];
 				            $file_name = md5($number.'_'.$index . rand(0, 9999)) . '.' . $fileext;		  
-				            copy($value, $path_app . '/filestore/documents/' . $file_name);
+				            copy($value, '/filestore/documents/' . $file_name);
 
 			                $source = new App\Source();
                             $source->path = '/filestore/documents/' . $file_name;
@@ -164,7 +164,7 @@ Route::get('parser', function(){
 	                		foreach( $files_url as $index=>$value){                			
 					        	$fileext = pathinfo($value)['extension'];
 					            $file_name = md5($number.'_'.$index . rand(0, 9999)) . '.' . $fileext;
-                                copy($value, $path_app . '/filestore/documents/' . $file_name);
+                                copy($value, '/filestore/documents/' . $file_name);
 				                $source = new App\Source();
 						        $source->path = '/filestore/documents/' . $file_name;
 						        $document->sources()->save($source);
